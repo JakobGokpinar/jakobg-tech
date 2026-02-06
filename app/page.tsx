f7467410f17d744aae17cdd758d68fc42ad3c85c
jakobgokpinar <jakobgokpinar@gmail.com>
2026-02-06 17:56:08 +0100

    small fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
 'use client'
 import { motion } from 'motion/react'
-import { XIcon } from 'lucide-react'
 import { Spotlight } from '@/components/ui/spotlight'
 import { Magnetic } from '@/components/ui/magnetic'
-import {
-  MorphingDialog,
-  MorphingDialogTrigger,
-  MorphingDialogContent,
-  MorphingDialogClose,
-  MorphingDialogContainer,
-} from '@/components/ui/morphing-dialog'
 import Link from 'next/link'
-import { AnimatedBackground } from '@/components/ui/animated-background'
 import {
   PROJECTS,
   WORK_EXPERIENCE,
@@ -255,7 +246,7 @@ export default function Personal() {
           ))}
         </div>
       </motion.section>
-      
+
       {/* Connect */}
       <motion.section
         variants={VARIANTS_SECTION}
